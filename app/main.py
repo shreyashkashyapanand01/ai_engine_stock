7543aeeb139c5f52faff72ef9d356fa43f8969b7
shreyashkashyapanand01 <shreyashkashyapanand01@gmail.com>
2026-03-18 00:57:48 +0530

    update : updated main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.stock_api import router as stock_router
 from app.api.scan_api import router as scan_router
 
 
-app = FastAPI(
-    title="AI Stock Intelligence Engine"
-)
+def create_app() -> FastAPI:
 
-app.include_router(stock_router)
-app.include_router(scan_router)
+    app = FastAPI(
+        title="AI Trading Coach Engine",
+        description="AI-powered stock analysis and market scanner",
+        version="1.0.0"
+    )
+
+    #  CORS (important for frontend later)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # later restrict this
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    #  Register routers
+    app.include_router(stock_router, prefix="")
+    app.include_router(scan_router, prefix="")
+
+    return app
+
+
+app = create_app()
